e29698268a6101cae4269991d4085ff89808ab4f
eduardin games <eduardo.pcavalcante@eaportal.org>
2024-06-09 20:09:24 +0000

    feat: instalação e inplantação do react-router-dom e do style-components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
 
 import { Home } from "./pages/Home"
-import { theme } from "./styles/theme.js"
+import { theme } from "./styles/theme"
 
 const router = createBrowserRouter ([
   {
@@ -12,9 +12,9 @@ const router = createBrowserRouter ([
 ])
 
 export  function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <RouterProvider router ={ router }/>
+ return (
+  <ThemeProvider theme={theme}>
+     <RouterProvider router ={ router }/>
     </ThemeProvider>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 
+
 export function Home() {
   return (
     <div>Home</div>
